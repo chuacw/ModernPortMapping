33de9c5e7c4a7825a05677deda35d2c48da527de
chuacw <1757930+chuacw@users.noreply.github.com>
2024-06-25 16:27:47 +0800

    Removed TorrentTracker

--- TorrentTracker.ts
+++
@@ -1,9 +0,0 @@
-// See https://github.com/webtorrent/bittorrent-dht
-// First, set discover the nearest InternetGatewayDevice
-// Using the IGD, open up torrent ports
-// Perform nslookup to figure out who the IP address belongs to
-// use nslookup -type=a "domain name" on DNS server
-// Record timestamp on IP address, send DHT query to IP address 
-// every "specified interval", eg, 5 secs.
-
-// See also: https://www.npmjs.com/package/@silentbot1/nat-api
